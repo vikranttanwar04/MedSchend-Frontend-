24767da5623b04f1eb1db3b33bd33640f137bdcb
Vikrant Tanwar <151028037+vikranttanwar04@users.noreply.github.com>
2025-11-20 23:04:24 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: "https://medschend-backend.onrender.com",
   withCredentials: true
 });
 
